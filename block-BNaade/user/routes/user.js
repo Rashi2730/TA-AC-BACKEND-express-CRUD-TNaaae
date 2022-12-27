e58ce21b91bb6dab311fc39186ac25fa1d3679f7
Rashi Israni <rashiisrani97@gmail.com>
2022-12-27 13:30:21 +0530

    user update

---
+++ block-BNaade/user/routes/user.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../models/user');
+
+router.get('/', (req, res) => {
+  User.find({}, (err, userr) => {
+    if (err) return next(err);
+    res.render('userList', { list: userr });
+  });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/', (req, res) => {
+  User.create(req.body, (err, data) => {
+    if (err) return res.redirect('/users/new');
+    if (data) return res.redirect('/');
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  var id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render('singleUser', { user: user });
+  });
+});
+
+router.get('/:id/edit', (req, res) => {
+  var id = req.params.id;
+  User.findById(id, (err, user) => {
+    if (err) return next(err);
+    res.render('userEdit', { user: user });
+  });
+});
+
+router.post('/:id', (req, res, next) => {
+  var id = req.params.id;
+  User.findByIdAndUpdate(id, req.body, (err, updatedUser) => {
+    if (err) return next(err);
+    res.redirect('/users/');
+  });
+});
+module.exports = router;
